37b94f012776b0aefbf2acf1e36854751e0081c9
Andrii Khlivnyi <akhlivnoy@gmail.com>
2025-04-02 01:29:10 +0300

    feat(home): [ARTX-31] add hero section

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,5 @@
 import './index.css';
+import './localization';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
