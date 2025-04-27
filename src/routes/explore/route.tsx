ac92ea5d92d803ff72ad5556258710d434916dc7
Vitalii Novosad <novosadvitalya@gmail.com>
2025-04-27 09:59:23 +0300

    refactor: Refactor `ExplorePage` to use `<ArtworkList>`

--- src/routes/explore/route.tsx
+++ src/routes/explore/route.tsx
@@ -2,9 +2,8 @@ import { useQuery } from '@tanstack/react-query';
 import { createFileRoute } from '@tanstack/react-router';
 
 import { getArtworks } from '@/api/endpoints/artworks.api';
-import { ArtworkCard } from '@/components/ArtworkCard';
-import { PaginationList } from '@/components/PaginationList';
-import { Spinner } from '@/components/Spinner';
+import type { Artwork } from '@/api/types/api.types';
+import { ArtworkList } from '@/components/ArtworkList';
 
 export const Route = createFileRoute('/explore')({
   component: ExplorePage,
@@ -16,7 +15,6 @@ export const Route = createFileRoute('/explore')({
 
 function ExplorePage() {
   const { page } = Route.useSearch();
-
   const pageSize = 6;
 
   const { data, isLoading, isError, isFetching } = useQuery({
@@ -25,38 +23,19 @@ function ExplorePage() {
     placeholderData: prev => prev,
   });
 
+  const artworks: Artwork[] = data?.items ?? [];
   const totalPages = data ? Math.ceil(data.total / pageSize) : 1;
 
   return (
-    <div className="flex flex-col gap-6 p-4">
-      {isLoading ? (
-        <div>Loading...</div>
-      ) : isError || !data ? (
-        <div>Failed to load artworks.</div>
-      ) : (
-        <>
-          <div className="relative">
-            {!isLoading && isFetching && (
-              <div className="absolute inset-0 z-10 flex items-center justify-center bg-white/80">
-                <Spinner />
-              </div>
-            )}
-
-            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3">
-              {data.items.map(artwork => (
-                <ArtworkCard
-                  artist={artwork.author}
-                  id={artwork.objectID}
-                  image={artwork.imageUrl}
-                  key={artwork.objectID}
-                  title={artwork.title}
-                />
-              ))}
-            </div>
-          </div>
-          <PaginationList currentPage={page} to="/explore" totalPages={totalPages} updatePageRange={true} />
-        </>
-      )}
-    </div>
+    <ArtworkList
+      updatePageRange
+      currentPage={page}
+      data={artworks}
+      isError={isError}
+      isFetching={isFetching}
+      isLoading={isLoading}
+      to={Route.to}
+      totalPages={totalPages}
+    />
   );
 }
